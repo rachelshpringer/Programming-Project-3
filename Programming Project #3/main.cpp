973e9b653285e0912418758bb086e1d8dc10c365
Rachel Shpringer <rshpringer@gmail.com>
2018-10-05 19:58:05 -0700

    Initial Commit

---
+++ Programming Project #3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Programming Project #3
+//
+//  Created by Rachel Shpringer on 10/5/18.
+//  Copyright © 2018 CS20A. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
